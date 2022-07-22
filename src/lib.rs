48ccba6bc7810c1e6c6d8b6eb51b72af7eef4e3f
pyama2000 <takahiko-yamashita@retty.me>
2022-07-22 22:32:37 +0900

    Generate the descriptor from bin

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
 pub mod kintai_service {
     tonic::include_proto!("kintai.v1");
+    pub const FILE_DESCRIPTOR_SET: &[u8] =
+        tonic::include_file_descriptor_set!("kintai_service_descriptor");
 }
