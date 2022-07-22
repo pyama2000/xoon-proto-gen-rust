829165d188a63a39d1d21bbc9803106955e40560
pyama2000 <takahiko-yamashita@retty.me>
2022-07-22 18:42:54 +0900

    Check crate version

---
+++ src/bin/check_crate_version.rs
@@ -0,0 +1,15 @@
+fn main() -> std::process::ExitCode {
+    let version = env!("CARGO_PKG_VERSION");
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        eprintln!("Wrong usage");
+        return 1.into()
+    }
+    let latest_tag = args.get(1).unwrap();
+    if version.eq(latest_tag) {
+        eprintln!("Update crate version!");
+        return 1.into()
+    }
+
+    0.into()
+}
