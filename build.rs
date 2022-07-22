a5dd16eaa658385ade8c5e8e3223511515d9becf
T.YAMASHITA <33086493+pyama2000@users.noreply.github.com>
2022-07-22 22:40:37 +0900

    Enable descriptor for kintai_service (#16)
    * Generate the descriptor file
    
    * Generate the descriptor from bin

--- build.rs
+++ build.rs
@@ -1,4 +1,10 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("xoon-proto/proto/kintai/v1/kintai_service.proto")?;
+    let out_dir = std::path::PathBuf::from(std::env::var("OUT_DIR")?);
+    tonic_build::configure()
+        .file_descriptor_set_path(out_dir.join("kintai_service_descriptor.bin"))
+        .compile(
+            &["xoon-proto/proto/kintai/v1/kintai_service.proto"],
+            &["xoon-proto/proto"],
+        )?;
     Ok(())
 }
