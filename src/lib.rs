c5000efd945881fbfcd26ccb50f0a8e0acd70541
T.YAMASHITA <33086493+pyama2000@users.noreply.github.com>
2022-07-24 19:10:04 +0900

    Rename kintai service file descriptor and package name (#19)
    * Rename to KINTAI_SERVICE_FILE_DESCRIPTOR_SET
    
    * Rename package name
    
    * Update package version
    
    * Revert "Update package version"
    
    This reverts commit a3a8e8d672df784416de760e9f3a955ac5fe62ad.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 pub mod kintai_service {
     tonic::include_proto!("kintai.v1");
-    pub const FILE_DESCRIPTOR_SET: &[u8] =
+    pub const KINTAI_SERVICE_FILE_DESCRIPTOR_SET: &[u8] =
         tonic::include_file_descriptor_set!("kintai_service_descriptor");
 }
