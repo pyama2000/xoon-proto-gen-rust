4e76a3dc59978b340cf87ed0fa9938c13f31c407
pyama2000 <takahiko-yamashita@retty.me>
2022-07-26 20:25:52 +0900

    Generate notification_service from proto

--- build.rs
+++ build.rs
@@ -6,5 +6,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             &["xoon-proto/proto/kintai/v1/kintai_service.proto"],
             &["xoon-proto/proto"],
         )?;
+    tonic_build::configure()
+        .file_descriptor_set_path(out_dir.join("notification_service_descriptor.bin"))
+        .compile(
+            &["xoon-proto/proto/notification/v1/notification_service.proto"],
+            &["xoon-proto/proto"],
+        )?;
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,9 @@ pub mod kintai_service {
     pub const KINTAI_SERVICE_FILE_DESCRIPTOR_SET: &[u8] =
         tonic::include_file_descriptor_set!("kintai_service_descriptor");
 }
+
+pub mod notification_service {
+    tonic::include_proto!("notification.v1");
+    pub const NOTIFICATION_SERVICE_FILE_DESCRIPTOR_SET: &[u8] =
+        tonic::include_file_descriptor_set!("notification_service_descriptor");
+}
