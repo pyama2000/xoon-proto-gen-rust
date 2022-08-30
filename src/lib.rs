54299be5783f69cf58e8d8d88b5de2991901090d
T.YAMASHITA <33086493+pyama2000@users.noreply.github.com>
2022-08-30 23:06:44 +0900

    Fix module name (#56)
    * Fix module name
    
    * Update xoon-proto-gen-rust to 0.1.13

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,17 @@
-pub mod kintai_service {
-    tonic::include_proto!("kintai.v1");
-    pub const KINTAI_SERVICE_FILE_DESCRIPTOR_SET: &[u8] =
-        tonic::include_file_descriptor_set!("kintai_service_descriptor");
+pub mod kintai {
+    pub mod v1 {
+        tonic::include_proto!("kintai.v1");
+        pub const KINTAI_SERVICE_FILE_DESCRIPTOR_SET: &[u8] =
+            tonic::include_file_descriptor_set!("kintai_service_descriptor");
+    }
 }
 
-pub mod notification_service {
-    tonic::include_proto!("notification.v1");
-    pub const NOTIFICATION_SERVICE_FILE_DESCRIPTOR_SET: &[u8] =
-        tonic::include_file_descriptor_set!("notification_service_descriptor");
+pub mod notification {
+    pub mod v1 {
+        tonic::include_proto!("notification.v1");
+        pub const NOTIFICATION_SERVICE_FILE_DESCRIPTOR_SET: &[u8] =
+            tonic::include_file_descriptor_set!("notification_service_descriptor");
+    }
 }
 
 pub mod automation {
